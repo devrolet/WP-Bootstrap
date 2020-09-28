fc4656f28a919f57a9e4862a09cc021569c0f62b
devrolet <chetdhill@gmail.com>
2020-09-28 18:27:36 -0400

    Add Parallax Scrolling Functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-let greeting = () => {
-  console.log('Hello World from main.js');
-};
+$(function() {
+  // Cache the window object
+  var $window = $(window);
+  // Parallax BG effect
+  $('section[data-type="background"]').each(function() {
+    var $bgobj = $(this);
+    
+    $(window).scroll(function() {
+      // scroll the bg at var speed
+      // the yPos is a negative value because we're scrolling up
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
 
-greeting();
+      // Final BG position
+      var coords = '50% '+ yPos + 'px';
+
+      // Move the BG
+      $bgobj.css({ backgroundPosition: coords });
+    });// end window scroll
+  });
+});
 
 
